1f4a22c9da909d3cd942eefde8db2a62e3a9382f
Naveen Jain <naveen1208jain@gmail.com>
2020-03-05 12:30:12 -0800

    if else run both

---
+++ March2/MarchDemo2.java
@@ -0,0 +1,21 @@
+package com.cts.training.Homework;
+import java.io.Console;
+public class MarchDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		String []arr= {"Naveen","Jain"};
+//		System.out.println(arr[0]);
+//		System.out.println(arr[1]);
+		
+		
+		String FirstName=args[0];
+		System.out.println(FirstName);
+		String LastName=args[1];
+		System.out.println(LastName);
+		
+	
+
+	} 
+
+}
---
+++ March2/RunBoth.java
@@ -0,0 +1,17 @@
+package com.cts.training.Homework;
+public class RunBoth {
+	public static void main(String[] args) {
+		if(args.length>0)
+		{
+			System.out.println("java");
+
+		}
+		else
+		{
+			System.out.println("I like");
+			String arr2[]= {"shyam"};
+			main(arr2);
+		}
+	}
+
+}
